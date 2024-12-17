2c9175adb59de3ece182269ca9e3dd93744f593f
Rajendra Prasad. sahoo <rajendra.sahoo@corp.csmpl.com>
2024-12-17 10:03:22 +0530

    new changes

--- src/main/java/com/project/manage/Service/HomeDetailsService.java
+++ src/main/java/com/project/manage/Service/HomeDetailsService.java
@@ -5,6 +5,7 @@ package com.project.manage.Service;
 
 import java.util.Map;
 
+import com.project.manage.Bean.HousedetailsBean;
 import com.project.manage.Bean.ResponseBean;
 import com.project.manage.Model.HomeDetails;
 
@@ -18,5 +19,9 @@ public interface HomeDetailsService {
 	ResponseBean gethomedetails(Long userid) throws Exception;
 
 	ResponseBean inactiveroomdetails(Long detailsid) throws Exception;
+
+	ResponseBean submitdetails(HousedetailsBean housedetailsbean) throws Exception;
+
+	ResponseBean getroomdetails(Long userid, Long housedetails) throws Exception;
 
 }
--- src/main/java/com/project/manage/ServiceImpl/HomeDetailsServiceImpl.java
+++ src/main/java/com/project/manage/ServiceImpl/HomeDetailsServiceImpl.java
@@ -3,13 +3,20 @@
  */
 package com.project.manage.ServiceImpl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.project.manage.Bean.HousedetailsBean;
 import com.project.manage.Bean.ResponseBean;
 import com.project.manage.Model.HomeDetails;
+import com.project.manage.Model.HouseRoomdetails;
 import com.project.manage.Repository.HomeDetailsRepository;
+import com.project.manage.Repository.HouseRoomdetailsRepository;
 import com.project.manage.Service.HomeDetailsService;
 
 /**
@@ -20,6 +27,10 @@ public class HomeDetailsServiceImpl implements HomeDetailsService{
 
 	@Autowired
 	private HomeDetailsRepository homeDetailsrepo;
+	
+	
+	@Autowired
+	private HouseRoomdetailsRepository homeroomdetailsrepo;
 	
 	@Override
 	public ResponseBean addhomedetails(HomeDetails homeDetails) throws Exception {
@@ -58,6 +69,43 @@ public class HomeDetailsServiceImpl implements HomeDetailsService{
 			bean.setStatus(HttpStatus.OK.value());
 			bean.setMessage("Success");
 		}catch (Exception e) {
+			throw new Exception(e);
+		}
+		return bean;
+	}
+
+	@Override
+	public ResponseBean submitdetails(HousedetailsBean housedetailsbean) throws Exception {
+		ResponseBean bean=new ResponseBean();
+		List<HouseRoomdetails> list=new ArrayList<>();
+		try {
+			for(HouseRoomdetails house:housedetailsbean.getRoomdetails()) {
+				house.setOwneruserid(housedetailsbean.getUserid());
+				house.setHouseid(housedetailsbean.getHousedetails());	
+				house.setAllotperson(house.getAllotperson()==""?null:house.getAllotperson());
+				house.setStatusflag(0);
+				house.setCreateon(new Date());
+				list.add(house);
+			}
+			homeroomdetailsrepo.saveAll(list);
+			bean.setStatus(HttpStatus.OK.value());
+			bean.setMessage("Success");
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+		return bean;
+	}
+
+	@Override
+	public ResponseBean getroomdetails(Long userid, Long housedetails) throws Exception {
+		ResponseBean bean=new ResponseBean();
+		List<HouseRoomdetails> list=new ArrayList<>();
+		try {
+			list=homeroomdetailsrepo.findByOwneruseridAndHouseid(userid,housedetails);
+			bean.setStatus(HttpStatus.OK.value());
+			bean.setRecord(list);
+			bean.setMessage("Success");
+		} catch (Exception e) {
 			throw new Exception(e);
 		}
 		return bean;
